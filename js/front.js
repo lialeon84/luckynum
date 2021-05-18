0f3983f8d8ef12481296f4d607ba40a972af2b89
Rose Vergel <50724307+lialeon84@users.noreply.github.com>
2021-05-18 09:55:55 -0500

    redid lucky bc the other one was an over kill

---
+++ js/front.js
@@ -0,0 +1,201 @@
+function myOne() {
+    document.getElementById("genone1").value = "";
+
+    var numRan = Math.floor(Math.random() * 25);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genone1").appendVal(numRan);
+}
+
+function myThree() {
+    document.getElementById("genthreeOne").value = "";
+
+    document.getElementById("genthree2").value = "";
+
+    document.getElementById("genthree3").value = "";
+
+    var numRan = Math.floor(Math.random() * 9);
+    var numRand = Math.floor(Math.random() * 9);
+    var numRando = Math.floor(Math.random() * 9);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genthreeOne").appendVal(numRan);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genthree2").appendVal(numRand);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genthree3").appendVal(numRando);
+}
+
+function myFour() {
+    document.getElementById("genfour1").value = "";
+
+    document.getElementById("genfour2").value = "";
+
+    document.getElementById("genfour3").value = "";
+
+    document.getElementById("genfour4").value = "";
+
+    var numRan = Math.floor(Math.random() * 9);
+    var numRand = Math.floor(Math.random() * 9);
+    var numRando = Math.floor(Math.random() * 9);
+    var numDmm = Math.floor(Math.random() * 9);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genfour1").appendVal(numRan);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genfour2").appendVal(numRand);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genfour3").appendVal(numRando);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genfour4").appendVal(numDmm);
+}
+
+function myFive() {
+    document.getElementById("genfive1").value = "";
+
+    document.getElementById("genfive2").value = "";
+
+    document.getElementById("genfive3").value = "";
+
+    document.getElementById("genfive4").value = "";
+
+    document.getElementById("genfive5").value = "";
+
+    var numRan = Math.floor(Math.random() * 35);
+    var numRand = Math.floor(Math.random() * 35);
+    var numRando = Math.floor(Math.random() * 35);
+    var numDmm = Math.floor(Math.random() * 35);
+    var numRnn = Math.floor(Math.random() * 35);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genfive1").appendVal(numRan);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genfive2").appendVal(numRand);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genfive3").appendVal(numRando);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genfive4").appendVal(numDmm);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genfive5").appendVal(numRnn);
+}
+
+function mySix() {
+    document.getElementById("gensix1").value = "";
+
+    document.getElementById("gensix2").value = "";
+
+    document.getElementById("gensix3").value = "";
+
+    document.getElementById("gensix4").value = "";
+
+    document.getElementById("gensix5").value = "";
+
+    var numRan = Math.floor(Math.random() * 52);
+    var numRand = Math.floor(Math.random() * 52);
+    var numRando = Math.floor(Math.random() * 52);
+    var numDmm = Math.floor(Math.random() * 52);
+    var numRnn = Math.floor(Math.random() * 52);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#gensix1").appendVal(numRan);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#gensix2").appendVal(numRand);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#gensix3").appendVal(numRando);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#gensix4").appendVal(numDmm);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#gensix5").appendVal(numRnn);
+}
+
+function mySeven() {
+    document.getElementById("genseven1").value = "";
+
+    document.getElementById("genseven2").value = "";
+
+    document.getElementById("genseven3").value = "";
+
+    document.getElementById("genseven4").value = "";
+
+    document.getElementById("genseven5").value = "";
+
+    var numRan = Math.floor(Math.random() * 69);
+    var numRand = Math.floor(Math.random() * 69);
+    var numRando = Math.floor(Math.random() * 69);
+    var numDmm = Math.floor(Math.random() * 69);
+    var numRnn = Math.floor(Math.random() * 69);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genseven1").appendVal(numRan);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genseven2").appendVal(numRand);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genseven3").appendVal(numRando);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genseven4").appendVal(numDmm);
+
+    $.fn.appendVal = function(TextToAppend) {
+        return $(this).val($(this).val() + TextToAppend);
+    };
+    $("#genseven5").appendVal(numRnn);
+}
